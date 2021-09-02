855b36b3e8e10153a23dd5a5ca7b68e499b1e105
pillose <pillose@pillose.kro.kr>
2021-09-02 20:20:08 +0900

    upload files 1

---
+++ overviewerConfig.js
@@ -0,0 +1,179 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "My World"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1630431479",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": " SW",
+            "zoomLevels": 9,
+            "defaultZoom": 1,
+            "maxZoom": 9,
+            "path": "render1",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "My World",
+            "last_rendertime": 1630422876,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                399,
+                5,
+                1003
+            ],
+            "minZoom": 0,
+            "spawn": [
+                399,
+                5,
+                1003
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "SE",
+            "zoomLevels": 9,
+            "defaultZoom": 1,
+            "maxZoom": 9,
+            "path": "render2",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "My World",
+            "last_rendertime": 1630422876,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                399,
+                5,
+                1003
+            ],
+            "minZoom": 0,
+            "spawn": [
+                399,
+                5,
+                1003
+            ],
+            "north_direction": 1
+        },
+        {
+            "name": "NW",
+            "zoomLevels": 9,
+            "defaultZoom": 1,
+            "maxZoom": 9,
+            "path": "render3",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "My World",
+            "last_rendertime": 1630422876,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                399,
+                5,
+                1003
+            ],
+            "minZoom": 0,
+            "spawn": [
+                399,
+                5,
+                1003
+            ],
+            "north_direction": 3
+        },
+        {
+            "name": "NE",
+            "zoomLevels": 9,
+            "defaultZoom": 1,
+            "maxZoom": 9,
+            "path": "render4",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "My World",
+            "last_rendertime": 1630422876,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                399,
+                5,
+                1003
+            ],
+            "minZoom": 0,
+            "spawn": [
+                399,
+                5,
+                1003
+            ],
+            "north_direction": 2
+        },
+        {
+            "name": "SW-Nighttime",
+            "zoomLevels": 9,
+            "defaultZoom": 1,
+            "maxZoom": 9,
+            "path": "render5",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "My World",
+            "last_rendertime": 1630422876,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                399,
+                5,
+                1003
+            ],
+            "minZoom": 0,
+            "spawn": [
+                399,
+                5,
+                1003
+            ],
+            "north_direction": 0
+        }
+    ]
+};
