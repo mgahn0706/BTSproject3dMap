f313d51dfc0aff7ac892020e320cf74df1b593bd
pillose <pillose@pillose.kro.kr>
2021-11-26 15:34:48 +0900

    update message

--- overviewer.js
+++ overviewer.js
@@ -303,9 +303,6 @@ overviewer.util = {
 
         overviewer.map = L.map('mcmap', {crs: L.CRS.Simple});
 
-        overviewer.map.attributionControl.setPrefix(
-            '<a href="https://overviewer.org">Overviewer/Leaflet</a>');
-
         overviewer.map.on('baselayerchange', function(ev) {
             
             // when changing the layer, ensure coordinates remain correct
